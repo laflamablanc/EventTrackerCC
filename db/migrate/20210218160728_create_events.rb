17eb158c2449c0aadbcf393e243748a2ccea5526
Sean LaFlam <sean.laflam@gmail.com>
2021-02-18 11:09:53 -0500

    Models created

---
+++ db/migrate/20210218160728_create_events.rb
@@ -0,0 +1,11 @@
+class CreateEvents < ActiveRecord::Migration[6.0]
+  def change
+    create_table :events do |t|
+      t.string :type
+      t.integer :actor_id
+      t.integer :repo_id
+
+      t.timestamps
+    end
+  end
+end
